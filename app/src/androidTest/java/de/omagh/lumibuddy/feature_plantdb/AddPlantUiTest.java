69c481e1f700be2928b0abb4d973a70606945c93
Omar Abidi <55606480+hiimOmagh@users.noreply.github.com>
2025-08-09 18:50:16 +0200

    Update AddPlantUiTest.java
    Extend instrumentation tests for plant saving

--- app/src/androidTest/java/de/omagh/lumibuddy/feature_plantdb/AddPlantUiTest.java
+++ app/src/androidTest/java/de/omagh/lumibuddy/feature_plantdb/AddPlantUiTest.java
@@ -2,9 +2,13 @@ package de.omagh.lumibuddy.feature_plantdb;
 
 import android.Manifest;
 
+import android.app.Activity;
+
 import androidx.test.espresso.Espresso;
 import androidx.test.espresso.action.ViewActions;
+import androidx.test.espresso.assertion.ViewAssertions;
 import androidx.test.espresso.contrib.RecyclerViewActions;
+import androidx.test.espresso.matcher.RootMatchers;
 import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
@@ -15,6 +19,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import de.omagh.lumibuddy.ui.MainActivity;
+import org.hamcrest.Matchers;
 
 @RunWith(AndroidJUnit4.class)
 public class AddPlantUiTest {
@@ -52,5 +57,26 @@ public class AddPlantUiTest {
                 .perform(RecyclerViewActions.scrollTo(
                         ViewMatchers.hasDescendant(ViewMatchers.withText("testPlantName"))
                 ));
+    }
+
+     @Test
+    public void addPlant_withBlankFields_showsToast() {
+        // Navigate to the plant list
+        Espresso.onView(ViewMatchers.withId(de.omagh.lumibuddy.R.id.plantListFragment))
+                .perform(ViewActions.click());
+
+        // Open add plant dialog
+        Espresso.onView(ViewMatchers.withId(de.omagh.feature_plantdb.R.id.addPlantFab))
+                .perform(ViewActions.click());
+
+        // Attempt to add plant without entering data
+        Espresso.onView(ViewMatchers.withText("Add")).perform(ViewActions.click());
+
+        // Assert toast indicating validation error
+        final Activity[] activity = new Activity[1];
+        activityRule.getScenario().onActivity(a -> activity[0] = a);
+        Espresso.onView(ViewMatchers.withText("Please enter name and type"))
+                .inRoot(RootMatchers.withDecorView(Matchers.not(Matchers.is(activity[0].getWindow().getDecorView()))))
+                .check(ViewAssertions.matches(ViewMatchers.isDisplayed()));
     }
 }
